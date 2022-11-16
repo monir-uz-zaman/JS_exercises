a2a9c75613c357c2c6e8ddbb28599869719ac0aa
Monir Uz Zaman <monir9317@gmail.com>
2022-11-16 23:56:11 +0200

    assignment JS function

---
+++ 10_Function_declaretion_expression/starter/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can Drive :D");
+
+// const interface = "Audio";
+// const private = 534;
+
+
+
+
+function logger() {
+    console.log(`My name is Jonas`);
+}
+
+// Calling/ running / invocing function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+const num = Number('23');
+
+
+*/
+
+function percetageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const percentageOfWorld = function (population) {
+    return (population / 7900) * 100;
+}
+
+
+const percPortugal1 = percetageOfWorld1(10);
+const percChina1 = percetageOfWorld1(1441);
+const percUSA1 = percetageOfWorld1(332);
+
+console.log(percPortugal1, percChina1, percUSA1);
+
